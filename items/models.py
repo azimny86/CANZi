5d5507de1584c123c98c2a6f0ed585852bca57fb
azimny86 <azimny86@gmail.com>
2023-07-06 13:20:44 +0000

    Add category model in models

--- items/models.py
+++ items/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
