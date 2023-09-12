45269debb1a5d814fff1a769680514a7582862cf
azimny86 <azimny86@gmail.com>
2023-09-12 15:02:13 +0000

    Add view of template

--- items/views.py
+++ items/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Items
 
-# Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = 'index.html'
